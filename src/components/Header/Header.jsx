8fe39c188f19e569438192150606e71678b0b7ca
tuit212 <asadbekanvarov9808@example.com>
2023-03-26 23:47:18 +0500

    header add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 import './Header.scss'
+import logo from '../../Assets/img/logo.svg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='site-header'>
+        <div className="container">
+            <div className="header">
+                <div className="header__logo">
+                    <Link to={'/'} >
+                        <img src={logo} alt="logo" />
+                    </Link>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
