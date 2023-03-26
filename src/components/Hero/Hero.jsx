f044b11b8ed4d217d65f08e73e4dbffda5a8104a
tuit212 <asadbekanvarov9808@example.com>
2023-03-27 00:53:37 +0500

    hero add

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Btn from '../../common/Btn/Btn'
+import './Hero.scss'
+
+const Hero = () => {
+  return (
+    <div className='hero-section'>
+        <div className="container">
+            <div className="hero">
+                <div className="hero__context">
+                    <h1>Masofaviy  ta'lim</h1>
+                    <p>
+                        Masofaviy o'quv kurslar orqali siz, eng so`ngi darsliklarni oson, qulay va mukammal o'rganishingiz mumkin.
+                    </p>
+                    <Btn/>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
